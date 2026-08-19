9f274e6d606c59f29651c0216779634e5abb8964
R-Velmurugan <r.velmurugan1506@gmail.com>
2026-08-19 21:53:29 +0530

    MYRCTC-22: completed the filter. Also wrote the repository as it was needed to complete the filter. The repository is part of MYRTC-16.

--- auth-service/src/main/java/com/myrctc/auth_service/auth/token/service/JwtService.java
+++ auth-service/src/main/java/com/myrctc/auth_service/auth/token/service/JwtService.java
@@ -2,7 +2,9 @@ package com.myrctc.auth_service.auth.token.service;
 
 import com.myrctc.auth_service.auth.token.Token;
 import com.myrctc.auth_service.auth.token.TokenConfig;
+import com.myrctc.auth_service.user.Email;
 import com.myrctc.auth_service.user.UserDto;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -12,17 +14,28 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.function.Function;
 
 @RequiredArgsConstructor
 @Service
 public class JwtService implements TokenService{
     private final TokenConfig tokenConfig;
 
+    private static final Integer TOKEN_INDEX_IN_HEADER = 7;
+
     @Override
+    @NonNull
     public Token getToken(@NonNull final UserDto user) {
         return new Token(generateToken(user));
     }
 
+    @Override
+    @NonNull
+    public Token getTokenFromHeader(@NonNull String authHeader) {
+        return new Token(authHeader.substring(TOKEN_INDEX_IN_HEADER)); //"Bearer ..." token starts from 7th index
+    }
+
+    @NonNull
     private String generateToken(@NonNull final UserDto user){
         final Date now = new Date();
         return Jwts.builder()
@@ -33,7 +46,38 @@ public class JwtService implements TokenService{
                 .compact();
     }
 
+    @NonNull
     private SecretKey getSigningKey(){
         return Keys.hmacShaKeyFor(tokenConfig.secret().getBytes(StandardCharsets.UTF_8));
+    }
+
+    @NonNull
+    @Override
+    public Email getUserEmail(@NonNull final Token token) {
+        return new Email(extractClaim(token, Claims::getSubject));
+    }
+
+    @Override
+    public boolean isTokenExpired(@NonNull final Token token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    @Override
+    public boolean isTokenValid(@NonNull final Token token, @NonNull final UserDto user) {
+        return getUserEmail(token).equals(user.getEmail());
+    }
+
+    @NonNull
+    private <T> T extractClaim(@NonNull final Token token, @NonNull final Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(getClaimsFromToken(token));
+    }
+
+    @NonNull
+    private Claims getClaimsFromToken(@NonNull final Token token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token.token())
+                .getPayload();
     }
 }
--- auth-service/src/main/java/com/myrctc/auth_service/user/UserDto.java
+++ auth-service/src/main/java/com/myrctc/auth_service/user/UserDto.java
@@ -2,10 +2,31 @@ package com.myrctc.auth_service.user;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @Getter
-public class UserDto {
+public class UserDto implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.getHashedPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail().email();
+    }
+
     Email email;
     String hashedPassword;
     Integer age;
