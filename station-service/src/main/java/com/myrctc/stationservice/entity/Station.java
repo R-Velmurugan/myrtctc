76cf9d33944a1fa271148286d85fb2dff336097f
R-Velmurugan <r.velmurugan1506@gmail.com>
2026-09-01 22:05:34 +0530

    MYRCTC-26: removed unused import

--- station-service/src/main/java/com/myrctc/stationservice/entity/Station.java
+++ station-service/src/main/java/com/myrctc/stationservice/entity/Station.java
@@ -1,6 +1,5 @@
 package com.myrctc.stationservice.entity;
 
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
