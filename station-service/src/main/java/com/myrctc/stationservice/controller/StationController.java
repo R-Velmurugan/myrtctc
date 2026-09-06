9c0d8a20c257ebbeb2eb976f649b36b4c40f5865
R-Velmurugan <r.velmurugan1506@gmail.com>
2026-09-06 19:04:13 +0530

    MYRCTC-26: added missing api call

--- station-service/src/main/java/com/myrctc/stationservice/controller/StationController.java
+++ station-service/src/main/java/com/myrctc/stationservice/controller/StationController.java
@@ -1,7 +1,9 @@
 package com.myrctc.stationservice.controller;
 
 import com.myrctc.stationservice.model.dto.StationDto;
+import com.myrctc.stationservice.service.StationService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class StationController {
+    private final StationService stationService;
 
     @PostMapping("/station")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<StationDto> createStation (@NonNull final @RequestBody StationDto stationDto) {
+        stationService.createStation(stationDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(stationDto);
     }
