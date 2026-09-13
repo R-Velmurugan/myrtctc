67b55945a9332f5a55fc0f21b66bc949e2cae353
R-Velmurugan <r.velmurugan1506@gmail.com>
2026-09-13 13:59:22 +0530

    MYRCTC-34: commented the booking service as migrations didnt run properly. Deleted local postgres and moved to containers.

--- booking-service/src/main/resources/db/migration/V2_create_train_users.sql
+++ booking-service/src/main/resources/db/migration/V2_create_train_users.sql
@@ -1,8 +1,8 @@
--- DROP TABLE users;+
-CREATE TABLE train_users
-(
-    email VARCHAR PRIMARY KEY REFERENCES users(email),
-    seat_preference  VARCHAR(1),
-    berth_preference VARCHAR(1),
-    saved_passengers JSONB
-)
+-- -- DROP TABLE users;+
+-- CREATE TABLE train_users
+-- (
+--     email VARCHAR PRIMARY KEY REFERENCES users(email),
+--     seat_preference  VARCHAR(1),
+--     berth_preference VARCHAR(1),
+--     saved_passengers JSONB
+-- )
